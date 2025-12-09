9e7d829e252344b3005beabd4662ea43d2ac796c
Eswaran <eswarana518@gmail.com>
2025-12-09 22:54:52 +0530

    build

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
-import ProductList from "@/components/ProductList";
-import Navbar from "../components/Navbar";
+"use client";
+
+import { useEffect, useState } from "react";
 import { fetchProducts } from "../lib/fetchProducts";
 import ClientHomePage from "@/components/ClientHomePage";
 
-export default async function HomePage() {
-  const products = await fetchProducts();
-  const logoSrc = products?.[0]?.image ?? null;
+export default function HomePage() {
+  const [products, setProducts] = useState([]);
+  const [logoSrc, setLogoSrc] = useState(null);
+
+  useEffect(() => {
+    async function load() {
+      const res = await fetchProducts();
+      setProducts(res);
+      setLogoSrc(res?.[0]?.image ?? null);
+    }
+    load();
+  }, []);
+
   return <ClientHomePage products={products} logoSrc={logoSrc} />;
 }
